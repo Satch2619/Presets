ff13ff9b85c90749dc311766c77dda5d3362942a
Satch2619 <ninho13@hotmail.de>
2022-09-10 22:02:47 +0200

    dist ordner erstellt

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import './../scss/style.scss'
+import javascriptLogo from './../javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
